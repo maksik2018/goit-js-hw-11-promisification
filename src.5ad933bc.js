0eef98ca317428f16e192fa2ff624debad80ab8b
maksik2018 <47830025+maksik2018@users.noreply.github.com>
2021-07-11 06:41:49 +0000

    Deploying to gh-pages from @ maksik2018/goit-js-hw-11-promisification-@e304b8902e78add34c1c0c30c78cfd09055d67da 🚀

--- src.5ad933bc.js
+++ src.5ad933bc.js
@@ -1,10 +1,12 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{}],"zIO9":[function(require,module,exports) {
+},{}],"u2QS":[function(require,module,exports) {
+const e=e=>new Promise(t=>{setTimeout(()=>{t(e)},e)}),t=e=>console.log(`Fulfilled after ${e}ms`);e(2e3).then(t),e(1e3).then(t),e(1500).then(t);
+},{}],"n9ff":[function(require,module,exports) {
 const e=[{name:"Mango",active:!0},{name:"Poly",active:!1},{name:"Ajax",active:!1}],a=(e,a)=>{const n=e.map(e=>e.name===a?{...e,active:!e.active}:e);return Promise.resolve(n)};a(e,"Mango").then(console.table),a(e,"Ajax").then(console.table);
-},{}],"rEyu":[function(require,module,exports) {
+},{}],"T9OW":[function(require,module,exports) {
 const o=(o,t)=>Math.floor(Math.random()*(t-o+1)+o),t=t=>new Promise((n,a)=>{const e=o(200,500);setTimeout(()=>{Math.random()>.3?n({id:t.id,time:e}):a(t.id)},e)}),n=({id:o,time:t})=>{console.log(`Transaction ${o} processed in ${t}ms`)},a=o=>{console.warn(`Error processing transaction ${o}. Please try again later.`)};t({id:70,amount:150}).then(n).catch(a),t({id:71,amount:230}).then(n).catch(a);
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss"),require("./partials/task2"),require("./partials/task3");const e=e=>new Promise(s=>{setTimeout(()=>{s(e)},e)}),s=e=>console.log(`Fulfilled after ${e}ms`);e(2e3).then(s),e(1e3).then(s),e(1500).then(s);
-},{"./sass/main.scss":"clu1","./partials/task2":"zIO9","./partials/task3":"rEyu"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-promisification-/src.cb6bae33.js.map
+"use strict";require("./sass/main.scss"),require("./js/task1"),require("./js/task2"),require("./js/task3");
+},{"./sass/main.scss":"clu1","./js/task1":"u2QS","./js/task2":"n9ff","./js/task3":"T9OW"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11-promisification-/src.5ad933bc.js.map
